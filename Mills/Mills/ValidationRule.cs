19425bbc12f47f5d4603a90bfe5b482c16d8aaec
Johannes Kolata <j.kolata@bcis.de>
2017-10-06 19:24:34 +0200

    Added ValidationRule

---
+++ Mills/Mills/ValidationRule.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Mills {
+  internal abstract class ValidationRule<T> {
+    private readonly Func<T, bool> _evaluator;
+
+    protected ValidationRule(Func<T, bool> evaluator) {
+      this._evaluator = evaluator;
+    }
+
+    public bool Evaluate(T element) {
+      return this._evaluator(element);
+    }
+  }
+}

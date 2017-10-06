bd787abad8cb215199754c7fb346155cf67911c0
Johannes Kolata <j.kolata@bcis.de>
2017-10-06 18:39:43 +0200

    Added MoveHistory

--- Mills/Mills/Board.cs
+++ Mills/Mills/Board.cs
@@ -1,11 +1,17 @@
-﻿namespace Mills {
+﻿using System.Collections.Generic;
+
+namespace Mills {
   class Board : IBoard {
     public Spot[][,] Spots { get; }
     private const int LevelCount = 3;
     private const int Dimensions = 3;
+    private readonly Stack<Move> _moveHistory;
+
+    public Move CurrentMove { get; private set; }
 
     public Board() {
       this.Spots = new Spot[LevelCount][,];
+      this._moveHistory = new Stack<Move>();
     }
 
     public void Initialize() {
@@ -17,6 +23,18 @@
           }
         }
       }
+    }
+
+    public void MakeMove(Move move) {
+      this.CurrentMove = move;
+    }
+
+    public void CommitMove() {
+      _moveHistory.Push(this.CurrentMove);
+    }
+
+    public void RollbackMove() {
+      this.CurrentMove = null;
     }
   }
 }
---
+++ Mills/Mills/Move.cs
@@ -0,0 +1,7 @@
+﻿namespace Mills {
+  internal class Move {
+    public Spot Source { get; set; }
+    public Spot Destination { get; set; }
+    public IPlayer Player { get; set; }
+  }
+}

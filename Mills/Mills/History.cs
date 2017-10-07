0a66fd9b6f690f5b56e6e418cf865e33f070230e
Nerzal <theel.tobias@gmx.de>
2017-10-07 12:31:13 +0200

    implemented history

--- Mills/Mills/History.cs
+++ Mills/Mills/History.cs
@@ -1,9 +1,20 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Mills {
-  public class History : IHistory {
-    public void Add(Move move) {
-      throw new NotImplementedException();
+    public class History : IHistory {
+        private readonly Stack<Move> _moves;
+
+        public History() {
+            this._moves = new Stack<Move>();
+        }
+
+        public History(Stack<Move> moves) {
+            this._moves = moves;
+        }
+
+        public void Add(Move move) {
+            this._moves.Push(move);
+        }
     }
-  }
 }

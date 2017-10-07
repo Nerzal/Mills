f8ccd843e6f84683024fcfca91671bff9aafb041
Nerzal <theel.tobias@gmx.de>
2017-10-07 21:15:42 +0200

    added summaries

--- Mills/Mills/History.cs
+++ Mills/Mills/History.cs
@@ -9,14 +9,24 @@ namespace Mills.ConsoleClient {
     public class History : IHistory {
         private readonly Stack<Move> _moves;
 
+        /// <summary>
+        /// ctor
+        /// </summary>
         public History() {
             this._moves = new Stack<Move>();
         }
 
+        /// <summary>
+        /// ctor
+        /// </summary>
+        /// <param name="moves"></param>
         public History(Stack<Move> moves) {
             this._moves = moves;
         }
-
+        /// <summary>
+        /// <inheritdoc cref="IHistory.Add"/>
+        /// </summary>
+        /// <param name="move"></param>
         public void Add(Move move) {
             this._moves.Push(move);
         }

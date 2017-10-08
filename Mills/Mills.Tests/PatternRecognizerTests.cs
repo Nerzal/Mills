1871a28795ef5142fe6dda4cd4d42df48ec1b46f
Nerzal <theel.tobias@gmx.de>
2017-10-08 20:21:54 +0200

    Added UnitTests for RowRecognizer

---
+++ Mills/Mills.Tests/PatternRecognizerTests.cs
@@ -0,0 +1,62 @@
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Mills.ConsoleClient.Board;
+using Mills.ConsoleClient.Board.Analyzer;
+
+namespace Mills.Tests {
+    [TestClass]
+    public class PatternRecognizerTests : BaseAnalyzerControllerTests {
+        [TestMethod]
+        public void FindAllRowsFor_2_PlayerOne2Rows() {
+            //Arrange
+            IPatternRecognizer recognizer = new PatternRecognizer();
+            //Create row 1
+            Coordinate row1Coordinate1 = new Coordinate(0, 0, 0);
+            this.Controller.Set(row1Coordinate1, this.Player);
+            Coordinate row1Coordinate2 = new Coordinate(0, 1, 0);
+            this.Controller.Set(row1Coordinate2, this.Player);
+            //Create row 2
+            Coordinate row2Coordinate1 = new Coordinate(2, 2, 0);
+            this.Controller.Set(row2Coordinate1, this.Player);
+            Coordinate row2Coordinate2 = new Coordinate(2, 2, 1);
+            this.Controller.Set(row2Coordinate2, this.Player);
+            Coordinate row2Coordinate3 = new Coordinate(2, 2, 2);
+            this.Controller.Set(row2Coordinate3, this.Player);
+            //Act
+            IEnumerable<Row> player1Rows = recognizer.FindAllRowsFor(this.Player);
+            //Assert
+            Assert.AreEqual(2, player1Rows.Count());
+
+            List<Row> player1RowsList = player1Rows.ToList();
+            Row firstRow = player1RowsList[0];
+            Assert.AreEqual(row1Coordinate1, firstRow.First);
+            Assert.AreEqual(row1Coordinate2, firstRow.Second);
+            Row secondRow = player1RowsList[1];
+            Assert.AreEqual(row2Coordinate1, secondRow.First);
+            Assert.AreEqual(row2Coordinate2, secondRow.Second);
+            Assert.AreEqual(row2Coordinate3, secondRow.Third);
+        }
+
+        [TestMethod]
+        public void FindAllMillsFor_1_PlayerOne() {
+            //Arrange
+            IPatternRecognizer recognizer = new PatternRecognizer();
+            Coordinate row2Coordinate1 = new Coordinate(2, 2, 0);
+            this.Controller.Set(row2Coordinate1, this.Player);
+            Coordinate row2Coordinate2 = new Coordinate(2, 2, 1);
+            this.Controller.Set(row2Coordinate2, this.Player);
+            Coordinate row2Coordinate3 = new Coordinate(2, 2, 2);
+            this.Controller.Set(row2Coordinate3, this.Player);
+            //Act
+            IEnumerable<Row> player1Rows = recognizer.FindAllRowsFor(this.Player);
+            //Assert
+            Assert.AreEqual(2, player1Rows.Count());
+
+            Row row = player1Rows.FirstOrDefault();
+            Assert.AreEqual(row2Coordinate1, row.First);
+            Assert.AreEqual(row2Coordinate2, row.Second);
+            Assert.AreEqual(row2Coordinate3, row.Third);
+        }
+    }
+}

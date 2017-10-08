655edea4dbd12741099403a2a98b993ada98b79a
Nerzal <theel.tobias@gmx.de>
2017-10-08 13:35:02 +0200

    Added base class for Analyzer and Controller tests

---
+++ Mills/Mills.Tests/BaseAnalyzerControllerTests.cs
@@ -0,0 +1,26 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Mills.ConsoleClient.Board;
+using Mills.ConsoleClient.Board.Analyzer;
+using Mills.ConsoleClient.Board.Controller;
+using Mills.ConsoleClient.Player;
+
+namespace Mills.Tests {
+    [TestClass]
+    public abstract class BaseAnalyzerControllerTests {
+        protected IBoard Board;
+        protected IBoardAnalyzer Analyzer;
+        protected IBoardController Controller;
+        protected Player Player;
+        protected Player Player2;
+
+        [TestInitialize]
+        public void InitializeTests() {
+            this.Board = new Board();
+            this.Analyzer = new BoardAnalyzer(this.Board);
+            this.Controller = new BoardController(this.Board);
+            this.Controller.Initialize();
+            this.Player = new Player("Nerzal") { Color = Colors.White };
+            this.Player2 = new Player("Wr4th0n") { Color = Colors.Black };
+        }
+    }
+}
---
+++ Mills/Mills.Tests/BoardControllerTests.cs
@@ -0,0 +1,8 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Mills.Tests {
+    [TestClass]
+    public class BoardControllerTests : BaseAnalyzerControllerTests {
+
+    }
+}

9b2a7334160aeb392556ad8f894b838a318de67f
Tobias Theel <t.theel@bcis.de>
2017-10-09 18:45:53 +0200

    even more summary

--- Mills/Mills/History.cs
+++ Mills/Mills/History.cs
@@ -2,33 +2,34 @@
 using Mills.ConsoleClient.GameController;
 
 namespace Mills.ConsoleClient {
+  /// <inheritdoc />
+  /// <summary>
+  /// Implementation of <see cref="T:Mills.ConsoleClient.IHistory" />
+  /// Holds a the history of every move that have been made in a single game
+  /// </summary>
+  public class History : IHistory {
+    private readonly Stack<Move> _moves;
+
     /// <summary>
-    /// Implementation of <see cref="IHistory"/>
-    /// Holds a the history of every move that have been made in a single game
+    /// ctor
     /// </summary>
-    public class History : IHistory {
-        private readonly Stack<Move> _moves;
-
-        /// <summary>
-        /// ctor
-        /// </summary>
-        public History() {
-            this._moves = new Stack<Move>();
-        }
-
-        /// <summary>
-        /// ctor
-        /// </summary>
-        /// <param name="moves"></param>
-        public History(Stack<Move> moves) {
-            this._moves = moves;
-        }
-        /// <summary>
-        /// <inheritdoc cref="IHistory.Add"/>
-        /// </summary>
-        /// <param name="move"></param>
-        public void Add(Move move) {
-            this._moves.Push(move);
-        }
+    public History() {
+      this._moves = new Stack<Move>();
     }
+
+    /// <summary>
+    /// ctor
+    /// </summary>
+    /// <param name="moves"></param>
+    public History(Stack<Move> moves) {
+      this._moves = moves;
+    }
+    /// <summary>
+    /// <inheritdoc cref="IHistory.Add"/>
+    /// </summary>
+    /// <param name="move"></param>
+    public void Add(Move move) {
+      this._moves.Push(move);
+    }
+  }
 }

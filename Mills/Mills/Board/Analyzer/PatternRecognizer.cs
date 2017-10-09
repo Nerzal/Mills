9475014c33c3b7b0e6c31fe283cde10e3f7b7fa7
Tobias Theel <t.theel@bcis.de>
2017-10-09 19:28:17 +0200

    was stimmt denn nicht mit dir?

--- Mills/Mills/Board/Analyzer/PatternRecognizer.cs
+++ Mills/Mills/Board/Analyzer/PatternRecognizer.cs
@@ -3,33 +3,45 @@ using System.Linq;
 using Mills.ConsoleClient.Player;
 
 namespace Mills.ConsoleClient.Board.Analyzer {
+  /// <inheritdoc />
+  /// <summary>
+  /// Implementation of <see cref="T:Mills.ConsoleClient.Board.Analyzer.IPatternRecognizer" />
+  /// Finds all rows and mills
+  /// </summary>
+  public class PatternRecognizer : IPatternRecognizer {
+    private IRowController _controller;
+
+    /// <inheritdoc />
+    public IBoard Board { get; }
+
     /// <summary>
-    /// Implementation of <see cref="IPatternRecognizer"/>
-    /// Finds all rows and mills
+    /// ctor
     /// </summary>
-    public class PatternRecognizer : IPatternRecognizer {
-        
-        /// <inheritdoc />
-        public IBoard Board { get; }
-
-        /// <summary>
-        /// ctor
-        /// </summary>
-        /// <param name="board"></param>
-        public PatternRecognizer(IBoard board) {
-            this.Board = board;
-        }
-
-        /// <inheritdoc />
-        public IEnumerable<Row> FindAllRowsFor(IPlayer player) {
-            ICollection<Row> result = new List<Row>();
-            
-            return result;
-        }
-
-        /// <inheritdoc />
-        public IEnumerable<Row> FindAllMillsFor(IPlayer player) {
-            return this.FindAllRowsFor(player).Where(row => row.IsMill);
-        }
+    /// <param name="board"></param>
+    /// <param name="controller"></param>
+    public PatternRecognizer(IBoard board, IRowController controller) {
+      this.Board = board;
+      this._controller = controller;
     }
+
+    /// <inheritdoc />
+    public IEnumerable<Row> FindAllRowsFor(IPlayer player) {
+      ICollection<Row> result = new List<Row>();
+      for (int level = 0; level < this.Board.LevelCount; level++) {
+        Row row = new Row();
+        for (int x = 0; x < this.Board.DimensionCount; x++) {
+          IPlayer occuppier = this.Board.Spots[level][x, 0].Player;
+          if (occuppier == player) {
+
+          }
+        }
+      }
+      return result;
+    }
+
+    /// <inheritdoc />
+    public IEnumerable<Row> FindAllMillsFor(IPlayer player) {
+      return this.FindAllRowsFor(player).Where(row => this._controller.IsMill(row));
+    }
+  }
 }
--- Mills/Mills/Board/Analyzer/Row.cs
+++ Mills/Mills/Board/Analyzer/Row.cs
@@ -1,29 +1,97 @@
-﻿using Mills.ConsoleClient.Player;
+﻿using System;
+using Mills.ConsoleClient.Player;
 
 namespace Mills.ConsoleClient.Board.Analyzer {
+  /// <summary>
+  /// Created by the PatternRecognizer to identify mills and possible mills
+  /// </summary>
+  public class Row {
     /// <summary>
-    /// Created by the PatternRecognizer to identify mills and possible mills
+    /// The Player to own this row
     /// </summary>
-    public class Row {
-        /// <summary>
-        /// The Player to own this row
-        /// </summary>
-        public IPlayer Player { get; set; }
-        /// <summary>
-        /// First element of the row
-        /// </summary>
-        public Coordinate? First { get; set; }
-        /// <summary>
-        /// Second element of the row
-        /// </summary>
-        public Coordinate? Second { get; set; }
-        /// <summary>
-        /// Third element of the row
-        /// </summary>
-        public Coordinate? Third { get; set; }
-        /// <summary>
-        /// A mill is found when there 3 coordinates in a row are occupied by the same player
-        /// </summary>
-        public bool IsMill => this.First != null && this.Second != null && this.Third != null;
+    public IPlayer Player { get; set; }
+    /// <summary>
+    /// First element of the row
+    /// </summary>
+    public Coordinate? First { get; set; }
+    /// <summary>
+    /// Second element of the row
+    /// </summary>
+    public Coordinate? Second { get; set; }
+    /// <summary>
+    /// Third element of the row
+    /// </summary>
+    public Coordinate? Third { get; set; }
+
+    /// <inheritdoc />
+    public override string ToString() {
+      return
+        $"Row occupied by {this.Player.Color}, Coordinates: First: {this.First} Second: {this.Second} Third: {this.Third}";
     }
+  }
+
+  /// <summary>
+  /// Controller for <see cref="Row"/>s
+  /// </summary>
+  public interface IRowController {
+    /// <summary>
+    /// Adds a coordinate into a row
+    /// </summary>
+    /// <param name="row">the row the coordinate will be added too</param>
+    /// <param name="coordinate">the coordinate to add</param>
+    void AddToRow(Row row, Coordinate coordinate);
+    /// <summary>
+    /// Checks if a row is a mill
+    /// </summary>
+    /// <param name="row">the row to check</param>
+    /// <returns>true if there are 3 coordinates in a row</returns>
+    bool IsMill(Row row);
+  }
+
+  /// <inheritdoc />
+  /// <summary>
+  /// Implementation of <see cref="IRowController"/>
+  /// </summary>
+  public class RowController : IRowController {
+    /// <inheritdoc />
+    public void AddToRow(Row row, Coordinate coordinate) {
+      if (row == null) {
+        throw new ArgumentNullException($"{nameof(row)} was null");
+      }
+
+      if (row.First == null) {
+        row.First = coordinate;
+      } else if (row.Second == null) {
+        row.Second = coordinate;
+      } else if (row.Third == null) {
+        row.Third = coordinate;
+      } else {
+        throw new RowAllreadyFullException(row, coordinate);
+      }
+    }
+
+    /// <inheritdoc />
+    public bool IsMill(Row row) {
+      if (row == null) {
+        throw new ArgumentNullException($"{nameof(row)} was null");
+      }
+
+      return row.First != null && row.Second != null && row.Third != null;
+    }
+  }
+
+  /// <inheritdoc />
+  /// <summary>
+  /// Used when AddToRow is called on a full row(mill)
+  /// </summary>
+  public class RowAllreadyFullException : Exception {
+    /// <inheritdoc />
+    /// <summary>
+    /// ctor
+    /// </summary>
+    /// <param name="row"></param>
+    /// <param name="coordinate"></param>
+    public RowAllreadyFullException(Row row, Coordinate coordinate) : base($"The row ") {
+    }
+  }
 }
